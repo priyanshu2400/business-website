868d610a7856c6221e90d06250a5ca979816cc6a
priyanshu2400 <priyanshujha024@gmail.com>
2023-11-02 00:30:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function cleartext() {
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+    document.getElementById("btn").innerText = "send successfully!";
+  }
